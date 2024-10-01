6d0cd5959d550235170b13fb273fc9acda52af7e
Abdul Aljawaab <abdulkhamis4041@gmail.com>
2024-10-01 21:39:13 +0300

    coded1st_findMthodLab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // code your solution here
+function superbowlWin(record) {
+    const winYears = record.filter(year => year.result === "W");
+    return winYears.length > 0 ? winYears[0].year : undefined;
+  }
